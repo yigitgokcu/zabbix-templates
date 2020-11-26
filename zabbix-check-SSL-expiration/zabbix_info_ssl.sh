613de143be41f9983c969867b36908edcc215a75
yigitgokcu <59539521+yigitgokcu@users.noreply.github.com>
2020-11-26 13:16:24 +0300

    Add files via upload

---
+++ zabbix-check-SSL-expiration/zabbix_info_ssl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOMAIN=$1
+
+if curl https://$DOMAIN/ &> /dev/null ; then
+
+EXP_DATE_STRING="$(openssl s_client -connect $DOMAIN:443 -servername  $DOMAIN </dev/null 2> /dev/n
+ull |  openssl x509 -in /dev/stdin  -noout -dates | grep notAfter | sed -e 's#notAfter=##')"
+
+EXP_DATE=$(date -d "$EXP_DATE_STRING" '+%s');
+
+CUR_DATE=$(date '+%s');
+
+TTL=$[ $EXP_DATE - $CUR_DATE ];
+
+echo -e "GOOD\t$TTL"
+
+else
+
+echo -e "BAD\t0"
+
+fi
