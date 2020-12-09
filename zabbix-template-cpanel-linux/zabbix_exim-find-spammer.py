25f734e03775609c1fd6af2b5c791ef77fe25c68
yigitgokcu <59539521+yigitgokcu@users.noreply.github.com>
2020-12-09 17:06:21 +0300

    Update zabbix_exim-find-spammer.py

--- zabbix-template-cpanel-linux/zabbix_exim-find-spammer.py
+++ zabbix-template-cpanel-linux/zabbix_exim-find-spammer.py
@@ -13,7 +13,7 @@ hostname = socket.gethostname()
 
 outgoing_count = os.popen("sudo /usr/sbin/exim -bpr | grep '<' | awk {'print $4'} | cut -d '<' -f 2 | cut -d '>' -f 1 | sort -n | uniq -c | sort -n  | sort -hr | head -n 10").read()
 
-mattermost_data = {'text': "\n " "\nTop Mail Senders - Possibly Spammers on " +str(hostname) + "\n " +str(outgoing_count)}
+mattermost_data = {'text': "\n " "\n :warning: Top Mail Senders - Possibly Spammers on " +str(hostname) + "\n " +str(outgoing_count)}
 
 response = requests.post(mattermost_url, data=json.dumps(mattermost_data),headers={'Content-Type': 'application/json'})
 #response = requests.post(mattermost_url, data=json.dumps(mattermost_data),headers={'Content-Type': 'application/json'}, verify=False)
